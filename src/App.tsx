cdc2d93336dc42c512fafb21b3cf877a0e1d0d48
Kelvin Jayanoris <kjayanoris@gmail.com>
2022-04-26 12:52:14 +0300

    Make the rpc uri a prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
+import { WalletError } from '@solana/wallet-adapter-base';
 import { WalletDialogProvider, WalletMultiButton } from '@solana/wallet-adapter-material-ui';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import {
     GlowWalletAdapter,
     PhantomWalletAdapter,
     SlopeWalletAdapter,
-    SolflareWalletAdapter,
-    TorusWalletAdapter,
+    SolletWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
 import { useSnackbar } from 'notistack';
+import { DEFAULT_RPC_URI } from './constants';
 import React, { FC, ReactNode, useCallback, useMemo } from 'react';
 import { Theme } from './Theme';
 
 export const App: FC = () => {
+    const rpcUri = DEFAULT_RPC_URI;
     return (
         <Theme>
-            <Context>
+            <Context rpcUri={rpcUri}>
                 <Content />
             </Context>
         </Theme>
     );
 };
 
-const Context: FC<{ children: ReactNode }> = ({ children }) => {
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = WalletAdapterNetwork.Devnet;
+interface ContextProps {
+    children: ReactNode;
+    rpcUri: string;
+}
 
-    // You can also provide a custom RPC endpoint.
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+const Context: FC<ContextProps> = ({ children, rpcUri }) => {
+    const endpoint = useMemo(() => rpcUri, [rpcUri]);
 
     // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
     // Only the wallets you configure here will be compiled into your application, and only the dependencies
@@ -38,10 +39,9 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
             new PhantomWalletAdapter(),
             new GlowWalletAdapter(),
             new SlopeWalletAdapter(),
-            new SolflareWalletAdapter({ network }),
-            new TorusWalletAdapter(),
+            new SolletWalletAdapter(),
         ],
-        [network]
+        []
     );
 
     const { enqueueSnackbar } = useSnackbar();
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
 
+export const DEFAULT_RPC_URI = 'https://api.mainnet-beta.solana.com';
 export const LAUNCH_PROGRAM_ID_V1 = new PublicKey('nova5ZKqeP5ck1ebcX8YFevUPmkW34cxRZxDSq267Gc');
 export const LAUNCH_PROGRAM_ID_V2 = new PublicKey('nva24Y1vHfhCrCLcqqFLXher9uZR4JjKP4D89MHhkmA');
