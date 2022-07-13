c6f98be634e20c9ab1af343f55e9a6375a34dd0c
Kelvin Jayanoris <kjayanoris@gmail.com>
2022-07-13 23:02:50 +0300

    Add V0 types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,20 @@
 import type { AnchorTypes } from '@saberhq/anchor-contrib';
+import { PublicKey } from '@solana/web3.js';
 import { IDL } from './idl';
 
 export type ProgramTypes = AnchorTypes<typeof IDL>;
 type Accounts = ProgramTypes['Accounts'];
 
 export type MasterAccount = Accounts['masterAccount'];
+
+export interface InnerV0Details {
+    programAddress: string;
+    programAuthority?: PublicKey;
+    recipients: string[];
+    seed: Buffer;
+    data: number[];
+}
+
+export interface V0Details {
+    [key: string]: InnerV0Details;
+}
